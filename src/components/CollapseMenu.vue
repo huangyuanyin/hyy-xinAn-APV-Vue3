718cd7580e3fa0c185a5b21513d3529e24659c2c
liuzhanghao <3443718488@qq.com>
2022-10-17 17:34:39 +0800

    菜单增加icon

--- src/components/CollapseMenu.vue
+++ src/components/CollapseMenu.vue
@@ -1,31 +1,17 @@
 <template>
   <el-aside>
-    <el-menu
-      :default-active="defaultActiveIndex"
-      class="el-menu"
-      :collapse="store.isCollapse"
-      active-text-color="rgb(85, 85, 85)"
-      background-color="rgb(245, 245, 245)"
-      text-color="rgb(51, 51, 51)"
-      router
-    >
+    <el-menu :default-active="defaultActiveIndex" class="el-menu" :collapse="store.isCollapse"
+      active-text-color="rgb(85, 85, 85)" background-color="rgb(245, 245, 245)" text-color="rgb(51, 51, 51)" router>
       <template v-for="(item, index) in menuList" :key="index + 'menu'">
         <!-- 一级菜单(无子级) -->
-        <el-menu-item
-          v-if="!item.children.length"
-          :index="item.path"
-          :disabled="item.isDisabled"
-        >
+        <el-menu-item v-if="!item.children.length" :index="item.path" :disabled="item.isDisabled">
           <el-icon>
-            <document v-if="!item.type" />
+            <component :is="item.icon" v-if="item.icon" />
           </el-icon>
           <template #title>{{ item.title }}</template>
         </el-menu-item>
         <!-- 一级菜单(有子级) -->
-        <el-sub-menu
-          v-if="item.children && item.children.length"
-          :index="item.id"
-        >
+        <el-sub-menu v-if="item.children && item.children.length" :index="item.id">
           <!-- 一级菜单模板区域 -->
           <template #title>
             <el-icon>
@@ -35,11 +21,7 @@
           </template>
           <el-menu-item-group>
             <!--二级菜单模板区域-->
-            <el-menu-item
-              v-for="(subItem, index) in item.children"
-              :key="index + 'subItem'"
-              :index="subItem.path"
-            >
+            <el-menu-item v-for="(subItem, index) in item.children" :key="index + 'subItem'" :index="subItem.path">
               <template #title>
                 <el-icon></el-icon>
                 <span>{{ subItem.title }}</span>
@@ -54,15 +36,10 @@
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
-import {
-  Document,
-  Menu as IconMenu,
-  Location,
-  Setting,
-} from "@element-plus/icons-vue";
+import { Document, Menu as IconMenu, School, DataLine, House } from "@element-plus/icons-vue";
 import { useAppStore } from "../store/modules/app/index";
 export default defineComponent({
-  components: { Document, IconMenu, Location, Setting },
+  components: { Document, IconMenu, School, House, DataLine },
   props: {
     menuList: {
       type: Array,
@@ -76,7 +53,7 @@ export default defineComponent({
       defaultActiveIndex.value = window.location.pathname
     });
     return {
-      store,defaultActiveIndex
+      store, defaultActiveIndex
     };
   },
 });
@@ -85,6 +62,7 @@ export default defineComponent({
 <style lang="scss" scoped>
 .el-menu {
   height: 100%;
+
   .el-menu-item.is-disabled {
     opacity: 100%;
     cursor: default;
@@ -95,13 +73,15 @@ export default defineComponent({
     padding: 0px;
   }
 
-  .is-active{
+  .is-active {
     background-color: #dedede;
   }
 }
+
 .el-menu:not(.el-menu--collapse) {
   width: 191px;
 }
+
 .el-aside {
   min-height: calc(100vh - 50px);
   background-color: rgb(245, 245, 245);
