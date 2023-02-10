7c480bd045c502487f5293974507b147e9ef6606
黄原寅 <3443718488@qq.com>
2023-02-10 10:41:38 +0800

    跳转带token

--- src/views/Layout/components/TopMenu.vue
+++ src/views/Layout/components/TopMenu.vue
@@ -90,6 +90,7 @@ const toLink = (index) => {
 
 const toWork = () => {
   window.location.href = 'http://10.4.150.55:8080/'
+  localStorage.setItem('token', sessionStorage.getItem('token'))
 }
 
 const changeDrawer = (drawer) => {
